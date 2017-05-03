dd9c90d9728f5e4895e9ed48d55aec0269b7ad81
Bas Jenneboer <bjenneboer@gmail.com>
2017-05-03 02:12:03 -0700

    Fix checkstyle errors in board.BoardTest

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -6,40 +6,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- *  Test a basic board
+ *  Test a basic board.
  *  @author Lars Ysla
  */
 class BoardTest {
 
-    
-    //	Check that a simple 1x1 board is valid
+    /**  Check that a simple 1x1 board is valid.
+     */
     @Test
     void testBasicSquare() {
-	
-	BasicSquare bs = new BasicSquare();
-	
-	Square[][] grid = {
-	    { bs }
-	};
-	
-	Board board = new Board(grid);
-	
-	assertTrue(board.invariant());
+        BasicSquare bs = new BasicSquare();
+        Square[][] grid = {{ bs }};
+        Board board = new Board(grid);
+        assertTrue(board.invariant());
     }
-    
-    //	Check to see if the square returned by the 1x1 board is the same square 
-    //	that was used to create the board.
+
+    /**
+     *  Check to see if the square returned by the 1x1 board is the same square
+     *  that was used to create the board.
+     */
     @Test
     void testSquareAt1() {
-	BasicSquare bs = new BasicSquare();
-	
-	Square[][] grid = {
-	    { bs }
-	};
-	
-	Board board = new Board(grid);
-	
-	assertThat(board.squareAt(0, 0)).isEqualTo(bs);
+        BasicSquare bs = new BasicSquare();
+        Square[][] grid = {{ bs }};
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0, 0)).isEqualTo(bs);
     }
-    
 }
