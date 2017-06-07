ba80e5d58e1e5842498aada520c43d32fd3f5df8
Bas Jenneboer <bjenneboer@gmail.com>
2017-06-07 12:09:50 +0200

    Make collision tests runnable on both collision classes (exercise 23)

---
+++ src/test/java/nl/tudelft/jpacman/level/DefaultPlayerInteractionMapTest.java
@@ -0,0 +1,11 @@
+package nl.tudelft.jpacman.level;
+
+/**
+ * Created by basjenneboer on 6/7/17.
+ */
+public class DefaultPlayerInteractionMapTest extends CollisionMapTest {
+
+    CollisionMap createMap() {
+        return new DefaultPlayerInteractionMap();
+    }
+}
