4d4d46c21e9cf7a27f57589168b02e6b3c4ff216
Lars Ysla <yslars@gmail.com>
2017-06-14 15:30:50 +0200

    Added MultiLevelGame class and adjusted MultiLevelLauncher so that it uses MultiLevelGame

--- src/main/java/nl/tudelft/jpacman/group81/MultiLevelLauncher.java
+++ src/main/java/nl/tudelft/jpacman/group81/MultiLevelLauncher.java
@@ -1,9 +1,16 @@
 package nl.tudelft.jpacman.group81;
 
+import nl.tudelft.jpacman.group81.multilevel.MultiLevelGame;
+import nl.tudelft.jpacman.game.GameFactory;
+import nl.tudelft.jpacman.level.Level;
+import org.checkerframework.checker.nullness.qual.EnsuresNonNull;
+
 /**
  *  @author Lars Ysla
  */
 public class MultiLevelLauncher extends MyExtension {
+
+    private MultiLevelGame multiGame;
 
     /**
      * Start a Multi-Level version of Pacman
@@ -12,5 +19,24 @@ public class MultiLevelLauncher extends MyExtension {
      */
     public static void main(String[] argv) {
         (new MultiLevelLauncher()).launch();
+    }
+
+    /**
+     * Creates a new game using the level from {@link #makeLevel()}.
+     *
+     * @return a new Game.
+     */
+    @EnsuresNonNull("game")
+    @Override
+    public MultiLevelGame makeGame() {
+        GameFactory gf = getGameFactory();
+        Level level = makeLevel();
+        multiGame = (MultiLevelGame) gf.createSinglePlayerGame(level);
+        return multiGame;
+    }
+
+    @Override
+    public MultiLevelGame getGame() {
+        return multiGame;
     }
 }
